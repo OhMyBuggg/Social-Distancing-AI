3fd51d3c6c6d2224c21faeff6260dee0628e4062
OhMyBuggg <sa0963278610@gmail.com>
2023-07-18 11:30:50 +0900

    new test

--- test_cuda_cv.py
+++ test_cuda_cv.py
@@ -1,18 +1,50 @@
-import cv2 as cv
+import sys
+import time
+import cv2
 
-vod = cv.VideoCapture(0)
+### VALUES
+NUM_REPEAT = 10000
 
-ret, frame = vod.read()
+### Read source image
+vid = cv2.VideoCapture(0)
+ret, frame = vid.read()
+if ret:
+    img_src = frame
+else:
+    print("Can not read from device 0")
 
-cv2.imshow("image", frame)
-cv2.waitKey(1)
+cv2.imshow('img_src', img_src)
+while True:
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-gpu_frame = cv.cuda_GpuMat()
+### Run with CPU
+time_start = time.time()
+for i in range (NUM_REPEAT):
+	img_dst = cv2.resize(img_src, (300, 300))
+time_end = time.time()
+print ("CPU = {0}".format((time_end - time_start) * 1000 / NUM_REPEAT) + "[msec]")
+cv2.imshow('CPU', img_dst)
+while True:
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-while ret:
-    gpu_frame.upload(frame)
+### Run with GPU
+img_gpu_src = cv2.cuda_GpuMat()	# Allocate device memory only once, as memory allocation seems to take time...
+img_gpu_dst = cv2.cuda_GpuMat()
+time_start = time.time()
+for i in range (NUM_REPEAT):
+	img_gpu_src.upload(img_src)
+	img_gpu_dst = cv2.cuda.resize(img_gpu_src, (300, 300))
+	img_dst = img_gpu_dst.download()
+time_end = time.time()
+print ("GPU = {0}".format((time_end - time_start) * 1000 / NUM_REPEAT) + "[msec]")
+cv2.imshow('GPU', img_dst)
+while True:
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    frame = cv.cuda.resize(gpu_frame, (352, 288))
-    frame.download()
+key = cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-    ret, frame = vod.read()
+print(cv2.cuda.getCudaEnabledDeviceCount())
